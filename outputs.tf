f53bbf38c622fdaf26f84f351f8d3acddad364ce
Brume <54155619+brumef2g2@users.noreply.github.com>
2021-04-19 12:35:45 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "catapp_url" {
   value = "http://${aws_eip.hashicat.public_dns}"
 }
 
-output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
-}
+#output "catapp_ip" {
+#  value = "http://${aws_eip.hashicat.public_ip}"
+#}
