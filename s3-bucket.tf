fcdeef030c1cae380b4cc3d9a535c5d8422fe0cb
Brume <54155619+brumef2g2@users.noreply.github.com>
2021-04-22 15:36:18 -0400

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/BRUME-Training/s3-bucket/aws"
+ # source  = "app.terraform.io/BRUME-Training/s3-bucket/aws"
+  source  = "app.terraform.io/BrumeOrg/s3-bucket/aws"
   version = "1.15.0"
   bucket_prefix = "brume2021"
 }
