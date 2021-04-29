555e8ad2ab023116d15e600934b50ba3c45cd531
Brume Oduaran <brume.oduaran@gmail.com>
2021-04-29 15:24:14 -0400

    update to output file

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "catapp_url" {
   value = "http://${aws_eip.hashicat.public_dns}"
 }
 
-#output "catapp_ip" {
-#  value = "http://${aws_eip.hashicat.public_ip}"
-#}
+output "catapp_ip" {
+  value = "http://${aws_eip.hashicat.public_ip}"
+}
 #
 #output "s3_bucket" {
 #  value = "http://${this_s3_bucket_bucket_regional_domain_name}"
