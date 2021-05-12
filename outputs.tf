75fdb8ae0fd8ee08a5983938dcbfa4127f011547
Brume <54155619+brumef2g2@users.noreply.github.com>
2021-05-12 16:28:03 -0400

    Revert "Added Clustering and tags"

--- outputs.tf
+++ outputs.tf
@@ -10,24 +10,3 @@ output "catapp_ip" {
 #output "s3_bucket" {
 #  value = "http://${this_s3_bucket_bucket_regional_domain_name}"
 #}
-
-#
-output "s3_bucket_regional_domain_name" {
-  description = "S3 Bucket information"
-  value = "http://${module.s3-bucket.this_s3_bucket_bucket_regional_domain_name}/"
-}
-
-output "s3_bucket_domain_name" {
-  description = "S3 Bucket Domain name"
-  value = "http://${module.s3-bucket.this_s3_bucket_bucket_domain_name}/"
-}
-
-output "s3_bucket_arn" {
-  description = "S3 Bucket Domain name"
-  value = "http://${module.s3-bucket.this_s3_bucket_arn}/"
-}
-
-output "s3_bucket_Bucket_ID" {
-  description = "S3 Bucket ID"
-  value = "http://${module.s3-bucket.this_s3_bucket_id}/"
-}
