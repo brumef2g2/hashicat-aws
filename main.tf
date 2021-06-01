e72189b45dae10465e94b8aca0fa38ce91708cb7
Brume <54155619+brumef2g2@users.noreply.github.com>
2021-06-01 19:34:08 -0400

    removed changes from last build

--- main.tf
+++ main.tf
@@ -125,8 +125,8 @@ resource "aws_instance" "hashicat" {
 
  tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops team"
-   Billable = "false"
+    #Department = "devops team"
+    #Billable = "false"
   
   }
 }
