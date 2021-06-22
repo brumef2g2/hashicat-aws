2c2cd0cc5fa3d84aec1b2c8069fae4cf1607dde4
Brume <54155619+brumef2g2@users.noreply.github.com>
2021-06-22 14:48:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,8 +125,8 @@ resource "aws_instance" "hashicat" {
 
  tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
-    Billable = "false"
+ #   Department = "devops"
+   # Billable = "false"
   
   }
 }
